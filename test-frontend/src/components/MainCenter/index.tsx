6e757218b98faa0fd76c897a29eb00f011489626
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2022-11-15 17:54:38 -0300

    feat: creation and styling of the container on the right

--- test-frontend/src/components/MainCenter/index.tsx
+++ test-frontend/src/components/MainCenter/index.tsx
@@ -1,10 +1,36 @@
-import { MainCenterStyle } from "./styles"
+import { MainCenterStyle,LeftContainer, RightContainer } from "./styles"
 const MainCenter=() =>{
 
     return (
         <MainCenterStyle>
-            oLa
+            <LeftContainer>
+                <h2>Simule sua Antecipação</h2>
+                <form action="">
+                    <label htmlFor="">Informe o Valor da venda <span>*</span></label>
+                    <input type="text" />
 
+                    <label htmlFor="">Em quantas parcelas <span>*</span></label>
+                    <input type="text" />
+
+                    <label htmlFor="">Informe o percentual de MDR <span>*</span></label>
+                    <input type="text" />
+                </form>
+            </LeftContainer>
+            
+            <RightContainer>
+                
+                    <h3>VOCÊ RECEBERÁ:
+                    <hr />
+                    </h3>
+                <p>Amanhã: <span>R$ 0,00</span></p>
+
+                <p>Em 15 dias: <span>R$ 0,00</span></p>
+
+                <p>Em 30 dias: <span>R$ 0,00</span></p>
+
+                <p>Em 90 dias: <span>R$ 0,00</span></p>
+                
+            </RightContainer>
         </MainCenterStyle>
     )
 }
--- test-frontend/src/components/MainCenter/styles.tsx
+++ test-frontend/src/components/MainCenter/styles.tsx
@@ -5,9 +5,61 @@ export const MainCenterStyle = styled.main`
     width           : 600px;
     height          : 400px;
     border-radius   : 3px;
-    border          : solid 1.5px var(--grey);
+    border          : solid 2px var(--grey);
     display         : flex;
     flex-direction  : row;
     align-items     : center;
     justify-content : center;
+
 `
+export const LeftContainer =styled.div`
+
+    width           : 60%;
+    height          : 100%;
+    display         : flex;
+    flex-direction  : column;
+    align-items     : center;
+    justify-content : center;
+
+    form{
+        display         : flex;
+        flex-direction  : column;
+        align-items     : center;
+        justify-content : center;
+
+    }
+
+`
+
+export const RightContainer =styled.div`
+    background-color: var(--colordireita);
+    width           : 40%;
+    height          : 100%;
+    display         : flex;
+    flex-direction  : column;
+    align-items     : flex-start;
+    justify-content : center;
+    color           : var(--blueMin);
+    gap             : 35px;
+    padding-left    : 25px;
+    
+    span{
+        font-weight  : bold  
+    }
+    hr{
+        width       : 120%;
+        color       : var(--blueMin);
+        margin-top  : 5px;
+        
+    }
+    h3{
+        color       : var(--blue);
+        font-style  : italic;
+
+    }
+    p,span{
+        font-style  : italic;
+
+    }
+`
+
